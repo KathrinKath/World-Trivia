d8918b648ac169f46e4ff45525bd0fd5ccf03d1d
KathrinKath <kathrin.peled@dci-student.org>
2024-10-08 13:26:34 +0200

    fixing logo

--- world-trivia-project/src/Components/navbar.jsx
+++ world-trivia-project/src/Components/navbar.jsx
@@ -1,12 +1,12 @@
 import "../App.css";
-import "../images/logo.gif";
+import logo from "/public/logo.gif";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       {/* <h1 className="navbar-title">Trivia Game</h1> */}
       <div className="logo-container">
-        <img src="./src/images/logo.gif" alt="logo " className="logo" />
+        <img src={logo} alt="logo " className="logo" />
       </div>
     </nav>
   );
